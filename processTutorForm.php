841c1b438a027b2660ae47561f66252f8685f9ca
Daniel Qian <daniel.j.qian@gamil.com>
2016-03-17 13:21:08 -0400

    First version of php

--- processTutorForm.php
+++ processTutorForm.php
@@ -4,7 +4,6 @@
 
 <body>
 
-<p>
 <?php
 	$servername = "localhost";
 	$username = "qiand";
@@ -15,24 +14,56 @@
 	$conn = new mysqli($servername, $username, $password, $dbName);
 	
 	
-	$id =(int) $_POST["ID"]
-	$name = $_POST["name"]
-	$email = $_POST["email"]
-	$isTutor = false; //Do post stuff
+	$id =(int) $_POST["ID"];
+	$name = $_POST["name"];
+	$email = $_POST["email"];	
+	$isTutor = $_POST["person"]=="tutor"?"Tutor":"Request"
+	
+	$tmpTimes = $_POST["times"];
+	$tmpSubjects = $_POST["subjects"];
+
+	$times = array();
+	$subkects = array();
+	
+	$count = 0;
+	for($i = 0; $i < count(tmpTimes); i++) {
+		if(tmpTimes[$i] == true) {	
+			$times[$count] = $timeKey[$i];
+		}
+	}
+	
+	$count = 0;
+	for($i = 0; $i < count(tmpSubjects); i++) {
+		if(tmpSubjects[$i] == true) {	
+			$subjects[$count] = $subjectKey[$i];
+		}
+
+	}
+
+
 	
 	
 	// Check connection 	
 	if ($conn->connect_error)
 		die("Connection failed: " . $conn->connect_error);
 	
-	$conn->query("DELETE FROM ".($isTutor?"Tutors":"Requests")." WHERE ID_number=".$id);
-	$conn->query("DELETE FROM ".($isTutor?"Tutor":"Request")."Times"."WHERE ID_number=".$id);
-	$conn->query("DELETE FROM ".($isTutor?"Tutor":"Request")."Subjects"."WHERE ID_number=".$id);
+	$conn->query("DELETE FROM ".($isTutor)."s"." WHERE ID_number=".$id);
+	$conn->query("DELETE FROM ".($isTutor)."Times"."WHERE ID_number=".$id);
+	$conn->query("DELETE FROM ".($isTutor)."Subjects"."WHERE ID_number=".$id);
+ 
+ 
+	$conn->query("INSERT INTO ".($isTutor)."s"." VALUES(".$id.",".$name.",".$email.")";
 
+	for($i = 0; $i < count(times); i++) {
+		$conn->query("INSERT INTO ".($isTutor)."Times"." VALUES(".$id.",".$times[$i].")";
+	}
 	
-	$conn->query("INSERT INTO");
-	$conn->query("DELETE FROM ".($isTutor?"Tutor":"Request")."Times"."WHERE ID_number=".$id);
-	$conn->query("DELETE FROM ".($isTutor?"Tutor":"Request")."Subjects"."WHERE ID_number=".$id);
+	for($i = 0; $i < count(subjects); i++) {
+		$conn->query("INSERT INTO ".($isTutor)."Times"." VALUES(".$id.",".$subjects[$i].")";
+	}
+
+
+	echo("Succesful sign up");
 
 	/*
 	$result = $conn->query("SELECT * FROM tutors");
@@ -44,7 +75,7 @@
 	$conn->close();
 	*/
 ?>
-</p>
+
 
 
 </body>
